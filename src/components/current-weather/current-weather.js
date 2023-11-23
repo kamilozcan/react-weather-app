fdfc29c6fd7e5565e5ca7811064729e7ca5d6959
Kamil Ozcan <kamilozcann@gmail.com>
2023-11-23 08:01:43 -0500

    Modified bottom

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -19,19 +19,19 @@ export const CurrentWeather = () => {
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Feels like</span>
-            <span className="parameter-label">22°C</span>
+            <span className="parameter-value">22°C</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Wind</span>
-            <span className="parameter-label">2 m/s</span>
+            <span className="parameter-value">2 m/s</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Humidity</span>
-            <span className="parameter-label">15%</span>
+            <span className="parameter-value">15%</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Pressure</span>
-            <span className="parameter-label">15 hPa</span>
+            <span className="parameter-value">15 hPa</span>
           </div>
         </div>
       </div>
