855044a88a67c24c636959fe424faa9eba4940bf
Kamil Ozcan <kamilozcann@gmail.com>
2023-11-23 07:10:07 -0500

    Created current-weather container

---
+++ src/components/current-weather/current-weather.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./current-weather.css";
+
+export const CurrentWeather = () => {
+  return (
+    <div className="weather">
+      <div className="top">
+        <div>
+          <p className="city">Belgrade</p>
+          <p className="weather-description">Sunny</p>
+        </div>
+        <img className="weather-icon" src="icon/01d.png" alt="weather" />
+      </div>
+    </div>
+  );
+};
