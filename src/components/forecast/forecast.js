4c3702ec77a2fcbf2ae421706ca5b068fed98daa
Kamil Ozcan <kamilozcann@gmail.com>
2023-11-24 17:55:37 -0500

    Created daily details

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -56,7 +56,34 @@ export const Forecast = ({ data }) => {
                 </div>
               </AccordionItemButton>
             </AccordionItemHeading>
-            <AccordionItemPanel></AccordionItemPanel>
+            <AccordionItemPanel>
+              <div className="daily-details-grid">
+                <div className="daily-details-grid-item">
+                  <label htmlFor="">Pressure</label>
+                  <label htmlFor="">{item.main.pressure} hPa</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label htmlFor="">Humidity</label>
+                  <label htmlFor="">{item.main.humidity}%</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label htmlFor="">Clouds</label>
+                  <label htmlFor="">{item.clouds.all}%</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label htmlFor="">Wind Speed</label>
+                  <label htmlFor="">{item.wind.speed} m/s</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label htmlFor="">Sea Level</label>
+                  <label htmlFor="">{item.main.sea_level}m</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label htmlFor="">Feels Like</label>
+                  <label htmlFor="">{Math.round(item.main.feels_like)}°C</label>
+                </div>
+              </div>
+            </AccordionItemPanel>
           </AccordionItem>
         ))}
       </Accordion>
