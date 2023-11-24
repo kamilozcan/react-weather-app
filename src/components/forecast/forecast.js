4ea13b0aa0b31d3d31face3fb1064de9410162d3
Kamil Ozcan <kamilozcann@gmail.com>
2023-11-24 16:50:32 -0500

    Created next 7 days daily weather and forecast.

---
+++ src/components/forecast/forecast.js
@@ -0,0 +1,65 @@
+import React from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemButton,
+  AccordionItemHeading,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+
+import "./forecast.css";
+
+const WEEK_DAYS = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+export const Forecast = ({ data }) => {
+  const dayInAWeek = new Date().getDay();
+  const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(
+    WEEK_DAYS.slice(0, dayInAWeek)
+  );
+
+  console.log(forecastDays);
+
+  return (
+    <>
+      <label className="title" htmlFor="">
+        Daily
+      </label>
+      <Accordion allowZeroExpanded>
+        {data.list.splice(0, 7).map((item, idx) => (
+          <AccordionItem key={idx}>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                <div className="daily-item">
+                  <img
+                    className="icon-small"
+                    src={`icon/${item.weather[0].icon}.png`}
+                    alt="weather"
+                  />
+                  <label className="day" htmlFor="">
+                    {forecastDays[idx]}
+                  </label>
+                  <label className="description" htmlFor="">
+                    {item.weather[0].description}
+                  </label>
+                  <label className="min-max" htmlFor="">
+                    {Math.round(item.main.temp_min)}°C /
+                    {Math.round(item.main.temp_max)}°C
+                  </label>
+                </div>
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel></AccordionItemPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
+  );
+};
